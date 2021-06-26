eb325eb74649a622a30b6acdef51e8ff4ce2c50f
john million <jpmillion@comcast.net>
2021-06-26 14:41:14 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,25 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    multiples = []
+    three_multiple = 3
+    five_multiple = 5
+    while three_multiple < limit || five_multiple < limit
+        if three_multiple < five_multiple
+            multiples.push(three_multiple)
+            three_multiple += 3
+        elsif five_multiple < three_multiple
+            multiples.push(five_multiple)
+            five_multiple += 5
+        else
+            multiples.push(three_multiple)
+            three_multiple += 3
+            five_multiple += 5
+        end
+    end
+    multiples
+end
+
+def sum_multiples(limit)
+    multiples = collect_multiples(limit)
+    multiples.reduce(0) { |sum, num| sum + num }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,32 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :limit, :multiples
+    def initialize(limit)
+        @limit = limit
+        @multiples = []
+    end
+
+    def collect_multiples
+        three_multiple = 3
+        five_multiple = 5
+        while three_multiple < limit || five_multiple < limit
+            if three_multiple < five_multiple
+                multiples.push(three_multiple)
+                three_multiple += 3
+            elsif five_multiple < three_multiple
+                multiples.push(five_multiple)
+                five_multiple += 5
+            else
+                multiples.push(three_multiple)
+                three_multiple += 3
+                five_multiple += 5
+            end
+        end
+        multiples
+    end
+
+    def sum_multiples
+        collect_multiples
+        multiples.reduce(0) { |sum, num| sum + num }
+    end
+end
